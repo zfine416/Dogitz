a9d5b18bab6b1770aac9b740379b3b5b32557dd9
Zachary Fine <zfine416@gmail.com>
2015-07-22 20:47:35 -0400

    add/setup voting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
-	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	before_action :find_post, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
 	before_action :authenticate_user!, except: [:index, :show]
+	
+
 	def index
 		@post = Post.all.order("created_at DESC")
 	end
@@ -38,6 +40,16 @@ class PostsController < ApplicationController
 	def destroy
 		@post.destroy
 		redirect_to root_path
+	end
+
+	def upvote
+		@post.upvote_by current_user
+		redirect_to :back
+	end
+
+	def downvote
+		@post.downvote_by current_user
+		redirect_to :back
 	end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-    resources :posts do
-      resources :comments
+  resources :posts do
+    member do
+      get "like", to: "posts#upvote"
+      get "dislike", to: "posts#downvote" 
     end
+    resources :comments
+  end
 
-    root 'posts#index'
+  root 'posts#index'
 end

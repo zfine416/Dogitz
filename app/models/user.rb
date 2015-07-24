6ca21f4bb67b296fcaeb3a5a3ba84d60bdcaed74
Zachary Fine <zfine416@gmail.com>
2015-07-24 18:20:16 -0400

    email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
 
+  validates :email, :email => true
+
   has_many :posts
   has_many :comments
 end

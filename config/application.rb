e52c73fd7e71f8ab00f8c53cac361a97eda66ef5
Zachary Fine <zfine416@gmail.com>
2015-08-19 17:33:16 -0400

    photo sizes

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,7 @@ module Dribble
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.exceptions_app = self.routes
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  get "/404", :to => "errors#not_found"
+  get "/422", :to => "errors#unacceptable"
+  get "/500", :to => "errors#internal_error"
+
   get '/posts/alltime', to: 'posts#alltime'
   get '/posts/week', to: 'posts#week'
   get '/posts/month', to: 'posts#month'

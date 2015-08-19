40de9a1cb1bee57fd8d24bc1a1bd88ec1a71250c
Zachary Fine <zfine416@gmail.com>
2015-08-19 17:39:09 -0400

    fds

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
 	default_scope { order(:cached_votes_up => :desc) } 
 	belongs_to :user
 	has_many :comments
-	has_attached_file :image, styles: { medium: "700x500#", small: "350x250#" }
+	has_attached_file :image, styles: { medium: "700x500", small: "350x250" }
 	validates_attachment_content_type :image, :content_type => /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)$/, :message => 'file type is not allowed (only jpeg/png/gif images)'
 	validates :image_fingerprint, :uniqueness => {:message => "Sorry. Someone else has already uploaded this exact image"}
 	validates :title, length: { maximum: 18,
